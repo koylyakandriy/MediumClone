ec30ff1f722d34a8dfcc3d537c441aa675fca3b9
koylyakandriy <koylyakandriy@gmail.com>
2020-03-30 11:18:00 +0300

    Updates

---
+++ precache-manifest.557af05bb25da8f26b2836079b92bb5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c377a812cc36656ee90ed3d7fad0abb",
+    "url": "/MediumClone/index.html"
+  },
+  {
+    "revision": "bb6d1a9f9792b49fd6eb",
+    "url": "/MediumClone/static/js/2.60bda3c7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MediumClone/static/js/2.60bda3c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d455e6c017bd216e09",
+    "url": "/MediumClone/static/js/main.8dd717d3.chunk.js"
+  },
+  {
+    "revision": "306ca0fddade55183b15",
+    "url": "/MediumClone/static/js/runtime-main.37db81df.js"
+  }
+]);
